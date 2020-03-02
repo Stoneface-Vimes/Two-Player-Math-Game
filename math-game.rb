229a27725d821481af432016480365c7744dbf03
Stoneface-Vimes <cliffweir92@gmail.com>
2020-03-02 13:30:36 -0500

    question logic works properly returns a question and an answer selects a random question type

--- math-game.rb
+++ math-game.rb
@@ -11,7 +11,7 @@ class Player
 end
 
 # Have 4 question functions
-# Add, subtract, multiply, divide
+# Add, subtract, multiply
 # use sample in question to select a random function from questions and pass in
 # the user defined numbers
 class Questions
@@ -26,29 +26,29 @@ class Questions
   def multiply(numbers)
     numbers[0] + numbers[1]
   end
-  # def divide(numbers)
-  #   return numbers[0] + numbers[1]
-  # end
 
-  def sendBack(numbers, answer, type)
+  def sendBack()
+    numbers = [rand(1...20), rand(1...20)]
+    type = ['add', 'subtract', 'multiply'].sample
     if type == 'add'
-      package = ["What is #{numbers[0]} + #{numbers[1]}?", answer]
+      package = ["What is #{numbers[0]} + #{numbers[1]}?", add(numbers)]
     elsif type == 'subtract'
-      package = ["What is #{numbers[0]} - #{numbers[1]}?", answer]
+      package = ["What is #{numbers[0]} - #{numbers[1]}?", subtract(numbers)]
     elsif type == 'multiply'
-      package = ["What is #{numbers[0]} x #{numbers[1]}?", answer]
+      package = ["What is #{numbers[0]} x #{numbers[1]}?", multiply(numbers)]
     end
+
+    return package
   end
 end
 
-
-
 p1 = Player.new
 p2 = Player.new
-
-
+p p1
+p1.lives = p1.lives - 1
 p p1
 
-p1.lives= p1.lives - 1
+question = Questions.new
 
-p p1
+test = question.sendBack()
+p test
